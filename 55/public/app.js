fa90b3742f283cd098724b2a778177c086c7eddb
sanketgadekar <sanketgadekar42@gmail.com>
2024-11-09 00:57:02 +0530

    Initial commit

---
+++ 55/public/app.js
@@ -0,0 +1,5 @@
+// public/app.js
+document.getElementById('clickMe').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'You clicked the button!';
+  });
+  
